ec5e6f82dad474d9c9b178a65466bd3e63265bf4
Rohan Kalvade Deshpande <rohanvdeshpande@hotmail.com>
2021-02-28 13:07:37 -0500

    Super simple add/subt updates

--- mathematics_dataset/super_simple_add_subtract.py
+++ mathematics_dataset/super_simple_add_subtract.py
@@ -7,8 +7,8 @@ def make_q_a(num1, num2):
 	return "{}\t{}\t1\tsuper_simple_add_sub".format(q, a)
 
 
-with open("ssas.tsv", "w") as fout:
-	for i in range(1000):
-		num1 = random.randint(0, 40)
-		num2 = random.randint(0, 40)
+with open("raw_data_tsv/super_simple_add_subtract.tsv", "w") as fout:
+	for i in range(100000):
+		num1 = random.randint(0, 100)
+		num2 = random.randint(0, 100)
 		print(make_q_a(num1, num2), file=fout)
